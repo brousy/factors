a94f50ed662f195662dbc6dc2e2107301290016c
sam <sammsfs@gmail.com>
2023-07-12 19:34:55 -0400

    gets info from movie database

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -1,36 +1,59 @@
 
 var inputEl = $("#actInput");
-var buttonEl =$("button");
+var buttonEl = $("button");
+
+
 
 function fetchRequests(newName) {
-    var tmdbURL = 'https://api.themoviedb.org/3/search/person?query='+newName+'&api_key=058146d0b6c44dd4946f65767dd0e064';
-    var celebURL = 'https://api.api-ninjas.com/v1/celebrity?name='+newName;
-    fetch(tmdbURL) .then(function (response) {
+    var tmdbURL = 'https://api.themoviedb.org/3/search/person?query=' + newName + '&api_key=058146d0b6c44dd4946f65767dd0e064'; //the movie database API URL
+    var celebURL = 'https://api.api-ninjas.com/v1/celebrity?name=' + newName; //the celebrity API URL
+    fetch(tmdbURL)
+        .then(function (response) {
             if (!response.ok) {
                 console.error(response);
             }
             return response.json();
-        }) .then(function (response) {
-            console.log(response);
-        })
-    
+        }).then(function (movieData) {
+            console.log(movieData);
+            showMovieInfo(movieData);
+        });
+
     fetch(celebURL, {
         method: 'GET',
         headers: {
             'X-API-Key': 'zFb0KKRElJvaciVwbAZvzw==oTpqLnCAfZPLw1Bk'
         }
-    }) .then(function (response){
+    }).then(function (response) {
         if (!response.ok) {
             console.error(response);
         }
         return response.json();
-    }) .then(function (response) {
+    }).then(function (response) {
         console.log(response);
-    })
+    });
+
+}
+
+function showMovieInfo(movieData){
+    var actor = movieData.results[0];
+    var movies = [];
+    movies = actor.known_for;
+    for(i=0; i<movies.length; i++){
+        var movie = movies[i].original_title;
+        console.log(movie);
+
+        var releaseDate = movies[i].release_date;
+        console.log(releaseDate);
+
+        var overview = movies[i].overview;
+        console.log(overview);
+    }
 }
 
 
-buttonEl.on('click',function(event){
+
+
+buttonEl.on('click', function (event) {
     event.preventDefault();
     handleSubmit();
 })
