a1a274a2f186efd7195f94793ce34a11a6f5d656
sam <sammsfs@gmail.com>
2023-07-14 16:47:57 -0400

    cleaned up some redundancy

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -7,26 +7,20 @@ var actorNameEl = $("#actorName");
 var actorImageEl = $(".card-content img");
 var heightEl = $(".card-content .col.s12:nth-child(2) h4");
 var birthdayEl = $(".card-content .col.s12:nth-child(3) h4");
+var actorName = "";
 
 buttonEl.on("click", function (event) {
   event.preventDefault();
-  handleSubmit();
+  actorName = inputEl.val();
+  var newName = nameFormatter(actorName);
+  fetchRequests(newName);
 });
 
-function handleSubmit() {
-  var actorName = inputEl.val();
-  console.log(actorName);
-  var newName = encodeURIComponent(actorName);
-  fetchRequests(newName);
-}
-
 function fetchRequests(newName) {
-  var tmdbURL =
-    "https://api.themoviedb.org/3/search/person?query=" +
-    newName +
-    "&api_key=" +
-    movieKey; //the movie database API URL
+  var tmdbURL = "https://api.themoviedb.org/3/search/person?query=" + newName + "&api_key=" + movieKey; //the movie database API URL
+
   var celebURL = "https://api.api-ninjas.com/v1/celebrity?name=" + newName; //the celebrity API URL
+
   fetch(tmdbURL)
     .then(function (response) {
       if (!response.ok) {
@@ -35,6 +29,7 @@ function fetchRequests(newName) {
       return response.json();
     })
     .then(function (movieData) {
+      console.log(movieData);
       showMovieInfo(movieData);
     });
 
@@ -91,12 +86,11 @@ function showMovieInfo(movieData) {
         );
 
       projectsEl.append(moviePosterElement);
-    }
+    } // we need an else function to handle if the media type is "tv"
   }
 }
 
 function showActorInfo(actorData) {
-  var actorName = nameFormatter(actorData);
   var actorHeight = metersToFeet(actorData);
   var actorBday = actorData[0].birthday;
 
@@ -115,33 +109,23 @@ function showActorInfo(actorData) {
   $(".card-content .birthday h4").text("Birthday: " + actorBday);
 }
 
-function nameFormatter(actorData) {
-  if (actorData && actorData.length > 0 && actorData[0].name) {
-    var fixNameArray = actorData[0].name.toLowerCase();
-    var nameArray = fixNameArray.split(" ");
-    var actorName = "";
-
-    for (var i = 0; i < nameArray.length; i++) {
-      var wordArray = nameArray[i].split("");
-      wordArray[0] = wordArray[0].toUpperCase();
-      actorName += wordArray.join("") + " ";
-    }
-
-    return actorName.trim();
-  } else {
-    return "";
+function nameFormatter(actorName) {
+  var nameArray = actorName.split(" ");
+  actorName = "";
+  for (var i = 0; i < nameArray.length; i++) {
+    var wordArray = nameArray[i].split("");
+    wordArray[0] = wordArray[0].toUpperCase();
+    actorName += wordArray.join("") + " ";
   }
+
+  return actorName;
 }
 
 function metersToFeet(actorData) {
-  if (actorData && actorData.length > 0 && actorData[0].height) {
-    var rawHeight = actorData[0].height * 3.28084;
-    var truncHeight = rawHeight - Math.trunc(rawHeight);
-    var actorHeight =
-      Math.floor(rawHeight) + "ft " + Math.ceil(truncHeight * 12) + "in";
+  var rawHeight = actorData[0].height * 3.28084;
+  var truncHeight = rawHeight - Math.trunc(rawHeight);
+  var actorHeight =
+    Math.floor(rawHeight) + "ft " + Math.ceil(truncHeight * 12) + "in";
 
-    return actorHeight;
-  } else {
-    return "";
-  }
+  return actorHeight;
 }
